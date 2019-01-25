0ba7d19b6d2547d91f15cdd4103c54b0ad948cb2
Carter Watts <cdwlions2016@gmail.com>
2019-01-24 21:15:27 -0500

    Lets try this again

--- src/main/java/neuralNetwork/FirstColumn.java
+++ src/main/java/neuralNetwork/FirstColumn.java
@@ -2,4 +2,39 @@ package neuralNetwork;
 
 public class FirstColumn {
 
+	
+	
+	public static void main(String[] args) {
+		
+		//Create hidden layer - Layer object
+		
+		
+		//Create output layer
+		
+		
+		//Define inputs 
+		double input[][] = new double[][]{
+				{1,0,0},
+				{1,0,1},
+				{0,0,0},
+				{0,1,1},
+				{0,0,1}
+		};
+		
+		//Define outputs
+		double output[][] = new double[][] {
+				{1},
+				{1},
+				{0},
+				{0},
+				{0}
+		};
+		
+		
+		
+		
+		
+		
+	}
+	
 }
--- src/main/java/neuralNetwork/Layer.java
+++ src/main/java/neuralNetwork/Layer.java
@@ -1,5 +1,34 @@
 package neuralNetwork;
 
+
+import java.util.function.Function;
+
 public class Layer {
 
+	 public enum ActivationFunctionType {
+	        SIGMOID,
+	        TANH
+	 }
+
+	 public enum InitialWeightType {
+		    RANDOM // only support random for the moment
+	 }
+
+	 double[][] weights;
+
+	 public final Function<Double, Double> activationFunction, activationFunctionDerivative;
+
+	 public Layer(int numberOfNeurons, int numberOfInputsPerNeuron) {
+	 	this(ActivationFunctionType.SIGMOID, InitialWeightType.RANDOM, numberOfNeurons, numberOfInputsPerNeuron);
+	 }
+
+	 public Layer(ActivationFunctionType activationFunctionType, int numberOfNeurons, int numberOfInputsPerNeuron) {
+		 this(ActivationFunctionType.SIGMOID, InitialWeightType.RANDOM, numberOfNeurons, numberOfInputsPerNeuron);
+	 }
+	 
+	 public Layer(ActivationFunctionType activationFunctionType, InitialWeightType initialWeightType, int numberOfNeurons, int numberOfInputsPerNeuron ) {
+
+
+	}
+	
 }
