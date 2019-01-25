1da4bc4276fa60c1ce4cba5a5fad93df17d79fe0
CarterTheGreat <cdwlions2016@gmail.com>
2019-01-24 23:54:46 -0500

    Working # pattern guesser

--- src/main/java/neuralNetwork/FirstColumn.java
+++ src/main/java/neuralNetwork/FirstColumn.java
@@ -7,34 +7,55 @@ public class FirstColumn {
 	public static void main(String[] args) {
 		
 		//Create hidden layer - Layer object
-		
+		Layer layer1 = new Layer(1,3);
 		
 		//Create output layer
-		
+		Network network = new Network(layer1);
 		
 		//Define inputs 
-		double input[][] = new double[][]{
-				{1,0,0},
-				{1,0,1},
-				{0,0,0},
-				{0,1,1},
-				{0,0,1}
+		double[][] inputs = new double[][]{
+            {0, 0, 1},
+            {1, 1, 1},
+            {1, 0, 1},
+            {0, 1, 1}
 		};
+
+		double[][] outputs = new double[][]{
+            {0},
+            {1},
+            {1},
+            {0}
+    	};
 		
-		//Define outputs
-		double output[][] = new double[][] {
-				{1},
-				{1},
-				{0},
-				{0},
-				{0}
-		};
+		System.out.println("Training the neural net...");
+        network.train(inputs, outputs, 10000);
+        System.out.println("Finished training");
+
+        System.out.println("Layer 1 weights");
+        System.out.println(layer1);
 		
 		
+        //Calculate predictions for unknown data
+        
+        //1,0,0
+        predict(new double[][]{{1, 0, 0}}, network);
+
+        // 0, 1, 0
+        predict(new double[][]{{0, 1, 0}}, network);
+
+        // 1, 1, 0
+        predict(new double[][]{{1, 1, 0}}, network);
 		
+	}
+	
+	public static void predict(double[][] testInput, Network network) {
+		network.process(testInput);
 		
-		
-		
+		System.out.println("Prediction on data "
+				+ testInput[0][0] + " "
+				+ testInput[0][1] + " "
+				+ testInput[0][2] + " -> "
+				+ network.getOutput()[0][0] + ", Expected: " + testInput[0][0]);
 	}
 	
 }
--- src/main/java/neuralNetwork/Layer.java
+++ src/main/java/neuralNetwork/Layer.java
@@ -26,9 +26,32 @@ public class Layer {
 		 this(ActivationFunctionType.SIGMOID, InitialWeightType.RANDOM, numberOfNeurons, numberOfInputsPerNeuron);
 	 }
 	 
-	 public Layer(ActivationFunctionType activationFunctionType, InitialWeightType initialWeightType, int numberOfNeurons, int numberOfInputsPerNeuron ) {
+	 public Layer(ActivationFunctionType activationFunctionType, InitialWeightType initialWeightType, int numberOfNeurons, int numberOfInputsPerNeuron) {
+	        weights = new double[numberOfInputsPerNeuron][numberOfNeurons];
 
+	        for (int i = 0; i < numberOfInputsPerNeuron; ++i) {
+	            for (int j = 0; j < numberOfNeurons; ++j) {
+	                if (InitialWeightType.RANDOM == initialWeightType) {
+	                    weights[i][j] = (2 * Math.random()) - 1; // shift the range from 0-1 to -1 to 1
+	                }
+	            }
+	        }
 
+	        if (ActivationFunctionType.TANH == activationFunctionType) {
+	            activationFunction = NetworkMath::tanh;
+	            activationFunctionDerivative = NetworkMath::tanhDerivative;
+	        } else {
+	            activationFunction = NetworkMath::sigmoid;
+	            activationFunctionDerivative = NetworkMath::sigmoidDerivative;
+	        }
+	    }
+
+	    public void adjustWeights(double[][] adjustment) {
+	        this.weights = NetworkMath.matrixAdd(weights, adjustment);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return MatrixUtil.matrixToString(weights);
+	    }
 	}
-	
-}
